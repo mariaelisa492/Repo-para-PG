4442c7aec3d7d93fefe7047eeeb8c00fff53ffee
Ivan Belasich <ivanbelasich@gmail.com>
2021-10-13 15:58:32 -0300

    conflictos terminados d 1 vez

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router';
-import { Login } from './pages/Login';
-import { Home } from './pages/Home'
+import {Login} from './pages/Login';
+import {Home} from './pages/Home'
+import {getProducts} from './redux/actions/index'
+import {useDispatch, useSelector} from 'react-redux'
 
-export default function App() {
+function App() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getProducts())
+  },[dispatch]) 
+
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
+      <Route exact path = "/" component={Login}/>
+      <Route exact path = "/home" component={Home}/>
       </Switch>
     </div>
   );
 }
 
+export default App;
