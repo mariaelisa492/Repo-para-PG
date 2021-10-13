28bb15b23360d5bbf0f9cda97e18f723c7c47ba0
Ivan Belasich <ivanbelasich@gmail.com>
2021-10-13 15:46:19 -0300

    conflictos 3

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Footer from "../components/Footer/Footer";
+import { ProductsList } from "../components/ProductsList/ProductsList";
 
 export const Home = () => {
 
-    const filteredProducts = useSelector((state) => state.filteredProducts)
+  const filteredProducts = useSelector((state) => state.filteredProducts)
 
-    // console.log(products, 'Estado de no modificable')
-    console.log(filteredProducts, 'Estado modificable')
+  // console.log(products, 'Estado de no modificable')
+  console.log(filteredProducts, 'Estado modificable')  
 
     return (
-        <div>
-            {filteredProducts.length > 0 ? <ProductsList filteredProducts={filteredProducts} /> : <p>loading</p>}
-        </div>
+      <div>
+       {filteredProducts.length > 0 ? <ProductsList filteredProducts={filteredProducts} /> : <p>loading</p>}
+     </div>
     );
+
+
 };
