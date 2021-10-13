e2efce105aeeab0d0029572d3505569cea068a16
Agustín Arenas <72575052+ArenasAgustin@users.noreply.github.com>
2021-10-13 11:19:43 -0300

    ruta formCreatePokemons

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router';
 import {Login} from './pages/Login';
-import {Home} from './pages/Home'
+import {Home} from './pages/Home';
+import FormCreateProducts from './components/FormCreateProducts/FormCreateProducts'; // modificar esta linea cuando se haga la page
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
       <Route exact path = "/" component={Login}/>
       <Route exact path = "/home" component={Home}/>
+      <Route exact path = "/create" component={FormCreateProducts}/>
       </Switch>
     </div>
   );
