2369c1077d374aadc54766a52ffdd6e695e7bd89
mec-nyan <marianoraspberrypi@gmail.com>
2021-10-13 13:51:35 -0300

    Alternative db for testing
    Set environment variables for testing in order not to pullute te local database.
    Implement conditional connection on 'db.js' to keep the database safe when testing.

--- Api/src/db.js
+++ Api/src/db.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();   
+// Import constants instead of accessing process.env directly
+const { CONNECTION_URL, CONNECTION_URL_DEV, NODE_ENV } = process.env;
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true})
+
+// Use an alternative db when testing so we don't pollute the original db
+const CONN = NODE_ENV === 'dev' ? CONNECTION_URL_DEV : CONNECTION_URL;
+
+mongoose.connect(CONN, { useNewUrlParser: true})
     .then(() => {
-        console.log('La conexión a mongodb se realizo correctamente')
+        console.log('La conexión a mongodb se realizo correctamente');
     })
     .catch((error) => console.log(`${error} did not connect`));
 
