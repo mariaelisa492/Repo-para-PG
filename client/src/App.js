1953f2cce18ca234953823b0ebd586e883025e03
Ivan Belasich <ivanbelasich@gmail.com>
2021-10-13 15:53:36 -0300

    conflictos resueltos

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router';
-import LoginRender from './pages/Login';
+import { Login } from './pages/Login';
 import { Home } from './pages/Home'
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={LoginRender} />
+        <Route exact path="/" component={Login} />
         <Route exact path="/home" component={Home} />
       </Switch>
     </div>
