499e6f7f5bf6e93289aac824826b8eb1ffe8d098
JJSolari <jignaciosolari@gmail.com>
2021-10-13 18:50:02 -0300

    ahoraSI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Login from "../components/Login/Login.jsx"
+import {Link} from 'react-router-dom'
 
 export default function LoginRender() {
     return (
         <div>
-            <Login />
+            <Link to="/home">
+                <h1>Soy login!!!!!!!</h1>
+            </Link>
         </div>
     )
 };
