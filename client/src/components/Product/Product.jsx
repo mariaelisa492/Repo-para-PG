ce4a178f8b6895413c6a4939f3e6fb516e6d1335
Idontplaytheguitar <aguskenny24@gmail.com>
2021-10-13 13:14:29 -0300

    Commit tardío

---
+++ client/src/components/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './product.css'
+export const Product = ({img, name, price, rating, isActive}) => {
+    
+    console.log(isActive)
+
+    return (
+        <div className='product'>
+            <img src={img}/>
+            <h4>{name}</h4>
+            <p>{price}</p>
+            <p>{rating}</p>
+        </div>
+
+    )
+}
