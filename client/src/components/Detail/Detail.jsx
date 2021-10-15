a4c225e820d5ea311fa2e34cfd6dabedad015d66
mec-nyan <marianoraspberrypi@gmail.com>
2021-10-15 01:42:54 -0300

    First mockup

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,11 +1,39 @@
 import './detail.css';
+import data from './data';
 
+const { name, description, image, stock, brand, model, price, category } = data[0];
+const summary = description.split('.')[0];
 
 export default function(props) {
 
   return (
     <div className='detail'>
-      <h1>Product detail</h1>
+
+      <div className='first'>
+        <h3>{category}</h3>
+        <h1>{name}</h1>
+        <p>{summary}</p>
+        <img src={image} alt='' />
+
+        <div className='price'>
+          <span>$ {price}</span>
+        </div>
+        <div className='cartBtns'>
+          <span className='lbl'>Add to cart</span>
+          <div className='btns'>
+            <span className='remove'>-</span>
+            <span className='quantity'>0</span>
+            <span className='add'>+</span>
+          </div>
+        </div>
+      </div>
+
+      <div className='second'>
+        <p>Brand: {brand} Model: {model}</p>
+        <hr />
+        <p>{description}</p>
+      </div>
+
     </div>
   );
 }
