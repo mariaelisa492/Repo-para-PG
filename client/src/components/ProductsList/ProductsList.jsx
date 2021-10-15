0abfddd44cdffd992eaf105df1547776ca0d48ef
mariaelisa492 <mariaelisa004@hotmail.com>
2021-10-14 22:15:38 -0500

    Cambios en el home y css de sus componentes

--- client/src/components/ProductsList/ProductsList.jsx
+++ client/src/components/ProductsList/ProductsList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Product } from "../Product/Product";
 import "./productsList.css";
 
@@ -7,41 +7,45 @@ export const ProductsList = ({ filteredProducts }) => {
 
   const [limit, setLimit] = useState(0);
 
-  function raise(){
-    if(limit + 6 <= filteredProducts.length){
+  function raise() {
+    if (limit + 6 <= filteredProducts.length) {
       setLimit(limit + 6)
     }
   }
 
-  function lower(){
-    if(limit - 6 >= 0){
+  function lower() {
+    if (limit - 6 >= 0) {
       setLimit(limit - 6)
     }
-    }
+  }
 
-    
+
   return (
-    <div className="containerList">
-      <div className="productList">
-        {filteredProducts.slice(limit, limit + 6).map((e) => {
-          return (
-            <Product
-              img={e.image}
-              name={e.name}
-              price={e.price}
-              rating={e.rating}
-              isActive={e.isActive}
-            />
-          );
-        })}
+    <>
+      <div className="containerList">
+        <div className="productList">
+          {filteredProducts.slice(limit, limit + 6).map((e) => {
+            return (
+              <Product
+                img={e.image}
+                name={e.name}
+                price={e.price}
+                rating={e.rating}
+                isActive={e.isActive}
+              />
+            );
+          })}
+        </div>
+        <div >
+          <div className="button_pagination">
+            <button onClick={lower} >Previous</button>
+            <button onClick={raise} >Next</button>
+          </div>
+        </div>
+        <div className="pagination">
+          <p>Showing {limit} to {limit + 6 < filteredProducts.length ? limit + 6 : filteredProducts.length} of {filteredProducts.length} products</p>
+        </div>
       </div>
-      <hr/>
-      <div className='buttonsContainer' >
-        <button onClick={lower} className='buttonss'>Previous 6</button>
-        <button onClick={raise} className='buttonss'>Next 6</button>
-      </div>
-        <p>showing {limit} to {limit + 6 < filteredProducts.length? limit +6 : filteredProducts.length} of {filteredProducts.length} products</p>
-
-    </div>
+    </>
   );
 };
