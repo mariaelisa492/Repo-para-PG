eb4ab27cc3c79f000f17ebd8923118fa3ea0d584
mec-nyan <marianoraspberrypi@gmail.com>
2021-10-17 19:11:18 -0300

    Drop down menu for mobile view
    Todavia hay mucho para hacer. Hice un componente separado para
    evitar conflictos por ahora.
    
    En modo retrato el menu lateral se oculta y le pasa la funcionalidad al menu movil.
    Despliega los submenus.
    
    Falta: que pase por debajo del navbar el tema que el carrusel tiene un z-index menor.
    Falta: animar los submenus para la proxima.
    Falta: calcular bien algunas medidas se va a romper en algunos dispositivos.
    Falta: ajustar el menu de usuario que por ahora se amontona si el usuario esta loguado y en modo movil (solo se ve se le dan click al icono)

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -3,9 +3,18 @@ import { useParams } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 import ProductDetail from "../components/Detail/Detail";
+import DropDownMenu from "../components/DropDownMenu/DropDownMenu";
 
 
 export const Detail = (props) => {
+
+  const [ showMenu, setShowMenu ] = useState(false);
+
+  function showDropDownMenu() {
+    setShowMenu(!showMenu);
+    console.log('drop down menu: ', showMenu);
+  }
+
 
   const id = useParams();
   console.log("Product id: ", id);
@@ -13,11 +22,13 @@ export const Detail = (props) => {
   return (
     <div>
       
+      <DropDownMenu showMenu={showMenu} showDropDownMenu={showDropDownMenu} />
+
       <div>
-        <NavBar />
+        <NavBar showDropDownMenu={showDropDownMenu} />
       </div>
 
-      <ProductDetail id={id} />
+      <ProductDetail id={id} showMenu={showMenu} showDropDownMenu={showDropDownMenu} />
 
       <div>
         <Footer/>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,22 +5,30 @@ import ProductsList from "../components/ProductsList/ProductsList";
 import NavBar from "../components/NavBar/NavBar";
 import SlideShow from "../components/SlideShow/SlideShow";
 import SideBar from "../components/SideBar/SideBar";
+import DropDownMenu from "../components/DropDownMenu/DropDownMenu";
 import './home.css';
 
 
 export const Home = () => {
   const filteredProducts = useSelector((state) => state.filteredProducts);
+  const [ showMenu, setShowMenu ] = useState(false);
+
+  function showDropDownMenu() {
+    setShowMenu(!showMenu);
+    console.log('drop down menu: ', showMenu);
+  }
 
   // console.log(products, 'Estado de no modificable')
-  console.log(filteredProducts, "Estado modificable");
+  // console.log(filteredProducts, "Estado modificable");
 
   return (
     <div>
       
       <div>
-        <NavBar />
+        <NavBar showDropDownMenu={showDropDownMenu} />
       </div>
       <div className='mainView' >
+        <DropDownMenu showMenu={showMenu} showDropDownMenu={showDropDownMenu} />
         <SideBar />
         <div className='slideAndProducts'>
           <div>
