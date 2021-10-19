0957814d96667a65323bc8327d6401a68bce2b0b
Idontplaytheguitar <aguskenny24@gmail.com>
2021-10-18 23:34:45 -0300

    lepush?

--- api/src/db.js
+++ api/src/db.js
@@ -15,12 +15,3 @@ mongoose.connect(CONN, { useNewUrlParser: true})
     .catch((error) => console.log(`${error} did not connect`));
 
 
-// funcionan como middlewere para conectar db
-// const MongoClient = require('mongodb').MongoClient;
-// var assert = require('assert')
-// const url = "mongodb+srv://30725507sm:<30725507sm>@cluster0.nczfs.mongodb.net/"
-
-// MongoClient.connect(url, function (err, client) {
-//     assert.equal(null, err)
-//     console.log('conectado a la base de datos')
-// })
