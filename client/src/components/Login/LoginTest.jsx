77b8c98de9c3ed2a6c13fcb0e3a88e71db36e72c
Yglagioia <gonzalolagioia@gmail.com>
2021-10-22 14:00:59 -0300

    ahora si

--- client/src/components/Login/LoginTest.jsx
+++ client/src/components/Login/LoginTest.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {useAuth0} from '@auth0/auth0-react'
 import './LoginTest.css'
-import {useDispatch} from 'react-redux'
 //import {searchUserInDb} from '../../redux/actions/index'
 
 export function LoginTest(){
