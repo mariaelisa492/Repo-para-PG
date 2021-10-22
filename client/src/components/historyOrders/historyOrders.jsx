db90e4a7a60fb515888ea02eee94aa36c47e50d2
JJSolari <jignaciosolari@gmail.com>
2021-10-21 23:45:45 -0300

    Order history - css name

--- client/src/components/historyOrders/historyOrders.jsx
+++ client/src/components/historyOrders/historyOrders.jsx
@@ -7,17 +7,17 @@ function HistoryOrders({ image, name, price, qty, brand  }) {
     console.log('ITEMS CARTTTTTTTTDECARTTTTTTT', image, name, price, qty)
 
     return (
-        <div className="item-container">
+        <div className="item-containerHistory">
           
           <div>
-            <img className="itemCartImg" src={image}  alt={"imagen de producto"} />
+            <img className="itemCartImgHistory" src={image}  alt={"imagen de producto"} />
           </div>
         
-            <div className="info-item">
-              <h3 className="itemName"> {name} </h3>
-              <h3 className="itemName"> {brand} </h3>
-              <h4 className="itemPrice">Price $  {price} </h4>
-              <h4 className="itemPrice">Quantity: {qty} </h4>
+            <div className="info-itemHistory">
+              <h3 className="itemNameHistory"> {name} </h3>
+              <h3 className="itemNameHistory"> {brand} </h3>
+              <h4 className="itemPriceHistory">Price $  {price} </h4>
+              <h4 className="itemPriceHistory">Quantity: {qty} </h4>
             </div>
             
         </div>
