2621502d4d38abb21d0bff4b617ef5ec2bea1198
mec-nyan <marianoraspberrypi@gmail.com>
2021-10-23 03:59:08 -0300

    fine tune navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,57 +1,91 @@
-import React, { useState } from "react";
 import "./navBar.css";
-import { BsFillCartFill, BsFillPersonFill } from "react-icons/bs"
+import { useState } from 'react';
 import logo from "../../images/waveMusic.png"
 import { Search } from "../Search/SearchBar";
-import {GiHamburgerMenu} from "react-icons/gi"
 import { LoginTest } from "../Login/LoginTest";
 import { Logout } from "../Login/LogoutTest";
-import {useAuth0} from "@auth0/auth0-react"
-import { NavLink } from "react-router-dom"; // a Link no se le pueden dar estilos...
-import { FaBars, FaUser, FaUserCheck, FaSearch } from 'react-icons/fa';
+import { useAuth0 } from "@auth0/auth0-react";
+import { NavLink } from "react-router-dom";
+import { FaBars, FaUser, FaSearch } from 'react-icons/fa';
 import { GrCart } from 'react-icons/gr';
 
 
 export default function NavBar({ showDropDownMenu }) {
+  // Este componente es demasiado grande!  Pero con el tiempo que tenemos
+  // prefiero no tocarlo por ahora, veremos la semana proxima si se puede
+  // limpiar y refactorear un poco.
 
-  const [showLinks, setShowLinks] = useState(false)
   const {isAuthenticated} = useAuth0()
+  const [ popup, setPopup ] = useState({
+    search: false,
+    login: false,
+  });
 
+  const pleaseLogin = e => {
+    e.preventDefault();
+    alert("Please login or register");
+  };
+
+  const showBar = () => setPopup({ ...popup, search: true });
+  const hideBar = () => setPopup({ ...popup, search: false });
 
   return (
     <nav className="navBar">
-      <label className="logo">
-        <NavLink to="/" className="active"><img src={logo} alt="logo"/></NavLink>
-      </label>
-
       <div className='landscape'>
-        <Search />
+        <label className="logo">
+          <NavLink to="/" className="active"><img src={logo} alt="logo"/></NavLink>
+        </label>
 
-        {isAuthenticated
-          ? <NavLink to='/profile' className='inactiveLink' activeClassName='activeLink' >
-              <FaUserCheck />
-            </NavLink>
-          :null}
+        <div className='searchBar'>
+          <Search />
+        </div>
 
-        {isAuthenticated
-          ? <NavLink to="/cart" className='inactiveLink' activeClassName='activeLink' >
-              <GrCart />
-            </NavLink>
-          :null}
+        <div className='userActions'>
+          {isAuthenticated
+            ? <NavLink to='/profile' activeClassName='activeLink'  >
+                <FaUser className='menuIcon' />
+              </NavLink>
+              : <FaUser className='menuIcon' onClick={e => pleaseLogin(e)} />}
 
-        {isAuthenticated ? <Logout/> : <LoginTest/>}
+          {isAuthenticated
+            ? <NavLink to="/cart" activeClassName='activeLink' >
+                <GrCart className='menuIcon' />
+              </NavLink>
+            : <GrCart className='menuIcon' onClick={e => pleaseLogin(e)} />}
+
+          {isAuthenticated ? <Logout/> : <LoginTest/>}
+        </div>
       </div>
 
-      <div className='mobileOptions'>
-          <FaSearch />
+      <div className='portrait'>
+        <label className="logo">
+          <NavLink to="/" className="active"><img src={logo} alt="logo"/></NavLink>
+        </label>
 
-          <FaUser  />
+        <div className={'popupSearchBar ' + (popup.search ? 'showSearch' : 'hideSearch')}>
+          <Search hideFunc={hideBar}/>
+          <p className='closeSearch' onClick={hideBar}>close</p>
+        </div>
 
-          <GrCart />
+        <div className='mobileOptions'>
+          <FaSearch className='noLink' onClick={showBar}/>
+          {isAuthenticated
+            ? <NavLink to='/profile' activeClassName='activeLink'  >
+                <FaUser className='menuIcon' />
+              </NavLink>
+              : <FaUser className='menuIcon' onClick={e => pleaseLogin(e)} />}
 
-          <FaBars onClick={showDropDownMenu}/>
+          {isAuthenticated
+            ? <NavLink to="/cart" activeClassName='activeLink' >
+                <GrCart className='menuIcon' />
+              </NavLink>
+            : <GrCart className='menuIcon' onClick={e => pleaseLogin(e)} />}
+
+          {isAuthenticated ? <Logout/> : <LoginTest/>}
+
+          <FaBars className='noLink' onClick={showDropDownMenu}/>
+        </div>
       </div>
-
     </nav>
   );
 }
