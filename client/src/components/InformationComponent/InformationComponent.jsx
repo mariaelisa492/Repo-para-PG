fcaa1dc4efbebe65977a77cab8c734ff8e06578f
“ArenasAgustin” <arenasagustin7@gmail.com>
2021-10-26 00:05:44 -0300

    mapa estatico

--- client/src/components/InformationComponent/InformationComponent.jsx
+++ client/src/components/InformationComponent/InformationComponent.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Map from './Map/Map';
 
 export default function InfoComponent() {
     return (
@@ -7,7 +6,17 @@ export default function InfoComponent() {
             <div><h1>Contact</h1></div>
 
             <div>
-                <div><Map /></div>
+                <div>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3175.9144539253894!2d-93.2850573!3d37.2497371!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87cf629e7ad3f35f%3A0xc7eb0fb7f8530349!2s742%20E%20Evergreen%20St%2C%20Springfield%2C%20MO%2065803%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1635216943191!5m2!1ses-419!2sar"
+                        width="600"
+                        height="450"
+                        allowfullscreen=""
+                        loading="lazy"
+                    >
+
+                    </iframe>
+                </div>
             </div>
 
             <div>
