ec3fdb1027ba2e9411f02d27ba5119e148050f0c
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2021-10-27 13:11:01 -0300

    Mail, order, newsletter, promos (#127)
    * uncommented .env: hay que sacar el puntoenv del repo
    
    * added: mail route and controllers
    
    * nodemailer, mail routes and controllers, env.js
    
    * Added order mail

---
+++ api/src/controllers/mail.js
@@ -0,0 +1,47 @@
+const nodemailer = require('nodemailer')
+const { user, pass, host } = require('./env')
+
+
+async function sendMail(req, res) {
+
+  const { to, subject, text, html } = req.body
+
+  let transporter = nodemailer.createTransport({
+    host: host,
+    port: 587,
+    secure: false,
+    auth: {
+      user: user,
+      pass: pass
+    },
+  })
+
+  try {
+    let info = await transporter.sendMail({
+      from: '"Wave Music" <wavemusicnotify@gmail.com>',
+      to,
+      subject,
+      text,
+      html: '<img src="https://res.cloudinary.com/dbu76tbs6/image/upload/v1635314355/samples/ecommerce/thankyousmall_zt76ka.png" alt="thanks!" />',
+    })
+
+    console.log("Message sent: %s", info.messageId)
+
+    res.json({message: "Susccess"})
+  } catch (err) {
+    console.error(err)
+    res.json({message: "Fail"})
+  }
+}
+
+
+const sendNewsletter = async (req, res) => {}
+
+const sendPromo = async (req, res) => {}
+
+
+module.exports = {
+  sendMail,
+  sendNewsletter,
+  sendPromo,
+}
