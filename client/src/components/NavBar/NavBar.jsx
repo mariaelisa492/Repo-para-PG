d7f5658fe3e51023dbd53172d64ac89fd2e87a16
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2021-10-30 23:08:13 -0300

    Icono del carrito muestra numero de items (#154)

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -48,21 +48,23 @@ export default function NavBar({ showDropDownMenu }) {
   const hadleChangeLogout = ()=>{
     localStorage.removeItem('items');
     logout({returnTo:window.location.origin})
-}
+  }
 
-const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-const about = useSelector(state => state.about);
+  const about = useSelector(state => state.about);
 
-const [inputAbout, setInputAbout] = useState(about);
+  const [inputAbout, setInputAbout] = useState(about);
 
-useEffect(() => {
-    dispatch(getAbout());
-}, [])
+  useEffect(() => {
+      dispatch(getAbout());
+  }, [])
 
-useEffect(() => {
-    setInputAbout(about)
-}, [about])
+  useEffect(() => {
+      setInputAbout(about);
+  }, [about])
+
+  const cart = useSelector(state => state.cart);
 
 
   return (
@@ -77,9 +79,11 @@ useEffect(() => {
         </div>
 
         <div className='userActions'>
-              <NavLink to="/cart" activeClassName='activeLink' >
-                <GrCart className='menuIcon' />
-              </NavLink>
+          <NavLink to="/cart" activeClassName='activeLink' >
+            <div className='cartIcon'>
+              <GrCart className='menuIcon' /><span className='itemCount' >{cart.length}</span>
+            </div>
+          </NavLink>
           
           {isAuthenticated &&
             <div className='profilePic' onClick={toggleUserOptions}>
@@ -105,11 +109,11 @@ useEffect(() => {
         <div className='mobileOptions'>
           <FaSearch className='noLink' onClick={showBar}/>
 
-          {isAuthenticated
-            ? <NavLink to="/cart" activeClassName='activeLink' className='profilePic' >
-                <GrCart className='menuIcon' />
-              </NavLink>
-            : <GrCart className='menuIcon' onClick={showDialog} />}
+          <NavLink to="/cart" activeClassName='activeLink' >
+            <div className='cartIcon'>
+              <GrCart className='menuIcon' /><span className='itemCount' >{cart.length}</span>
+            </div>
+          </NavLink>
 
           {isAuthenticated &&
             <div className='profilePic' onClick={toggleUserOptions}>
