d5f59ac6529afb68db2674dd65836e87fec12a11
Ivan Belasich <ivanbelasich@gmail.com>
2021-11-03 03:16:36 -0300

    ahora si , funca

--- client/src/components/PrivateRoute/PrivateRoute.jsx
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -5,48 +5,47 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { useSelector, useDispatch } from 'react-redux';
 import { searchUserInDb } from '../../redux/actions';
 
+const PrivateRoute = ({ component: Component, rest }) => {
 
-const PrivateRoute = ({ component:Component, rest}) => {
+  const dispatch = useDispatch()
+  const { user } = useAuth0();
 
-    const dispatch = useDispatch()
-    const { user } = useAuth0();
+  const userRedux = useSelector(state => state.user)
+  const role = userRedux.user ? userRedux.user[0]?.role : null
 
-    const userRedux = useSelector(state => state.user)
-    const role = userRedux.user ? userRedux.user[0]?.role : null
-    
-    useEffect(() => {
-     dispatch(searchUserInDb(user?.email))
-    }, [user] ) 
+  useEffect(() => {
+    dispatch(searchUserInDb(user?.email))
+  }, [user])
 
 
-    return (
-        <Route {...rest}> {role 
-          ? <Component /> 
-          : <div style={{
-            backgroundColor: '#d0d0d0',
-            color: '#6f6f6f',
-            height: '100vh',
-            width: '100vw',
-            textAlign: 'center',
-            }}>
-            <h5 style={{
-              padding: '200px 0px 50px',
-              textAlign: 'center',
-              fontWeight: 'normal',
-            }}>You are not allowed to perform this action</h5>
-            <NavLink to='/' >
-              <button style={{
-                width: '200px',
-                backgroundColor: 'orange',
-                color: 'black',
-                cursor: 'pointer',
-                borderRadius: '8px',
-                margin: 'auto',
-                boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.5)',
-              }}>Back</button>
-            </NavLink>
-            </div> } </Route>
-    )
+  return (
+    <Route {...rest}> {role === "ROLE_ADMIN"
+      ? <Component />
+      : <div style={{
+        backgroundColor: '#d0d0d0',
+        color: '#6f6f6f',
+        height: '100vh',
+        width: '100vw',
+        textAlign: 'center',
+      }}>
+        <h5 style={{
+          padding: '200px 0px 50px',
+          textAlign: 'center',
+          fontWeight: 'normal',
+        }}>You are not allowed to perform this action</h5>
+        <NavLink to='/' >
+          <button style={{
+            width: '200px',
+            backgroundColor: 'orange',
+            color: 'black',
+            cursor: 'pointer',
+            borderRadius: '8px',
+            margin: 'auto',
+            boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.5)',
+          }}>Back</button>
+        </NavLink>
+      </div>} </Route>
+  )
 };
 
 export default PrivateRoute;
