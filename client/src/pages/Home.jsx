c8a508ecddc5616857bb3bdb5ca5ec3288bcff89
Juani Solari <jignaciosolari@gmail.com>
2021-11-04 19:28:23 -0300

    not found products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,6 +19,8 @@ export const Home = () => {
   const products = useSelector((state) => state.products)
   const filteredTF = useSelector((state) => state.filteredTF)
 
+  console.log('SOOOOOOY FILTEREDPRODUCST DE HOME', filteredProducts)
+
   const {user, isAuthenticated} = useAuth0()
 
   console.log(filteredTF, 'FILTEREDTF')
