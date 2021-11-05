0eada386d54d379323512e933eed20161557ca16
idontplaytheguitar <aguskenny24@gmail.com>
2021-11-04 22:15:22 -0300

    ups

--- client/src/redux/constants/index.js
+++ client/src/redux/constants/index.js
@@ -1,5 +1,5 @@
 // ----------------- LINKS
-export const LOCALHOST_URL = "http://localhost:5000"
+export const LOCALHOST_URL = "https://herokupftest.herokuapp.com    "
 
 // ----------------- TYPES FOR REQUESTS
 export const GET_PRODUCTS = "GET_PRODUCTS"
